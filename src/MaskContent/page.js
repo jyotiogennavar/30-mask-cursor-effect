21998f21028fa0e8b7926426b361f533ba725de0
Jyoti <jyoti.ogennavar@xecurify.com>
2024-04-30 12:22:48 +0530

    Update content in page.js

--- src/MaskContent/page.js
+++ src/MaskContent/page.js
@@ -21,14 +21,16 @@ export default function Home() {
         transition={{ type: "tween", ease: "backOut", duration:0.5}}
       >
           <p onMouseEnter={() => {setIsHovered(true)}} onMouseLeave={() => {setIsHovered(false)}}>
-            A visual designer - with skills that haven't been replaced by A.I (yet) - making good shit only if the paycheck is equally good.
+            A visual developer - with skills that haven't been replaced by A.I (yet) - making good shit only if the paycheck is equally good.
           </p>
       </motion.div>
 
       <div className={styles.body}>
-        <p>I'm a <span>selectively skilled</span> product designer with strong focus on producing high quality & impactful digital experience.</p>
+        <p>I'm a <span>creative</span> web developer with strong focus on producing high quality & impactful digital experience.</p>
       </div>
 
     </main>
   )
 }
+
+
